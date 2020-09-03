90c717739fcc61b0265046e2b5fc65667e9ea44b
pis <pis.ceyatta@gmail.com>
2020-09-03 23:56:49 +0900

    時間間隔の設定

--- chatapp/public/javascripts/publish.js
+++ chatapp/public/javascripts/publish.js
@@ -1,8 +1,26 @@
 'use strict';
-let last_send = Date.now();
+
+let interval_time = 1000; // ミリ秒
+let last_send = 0;
+let isPost = false;
 
 // 投稿メッセージをサーバに送信する
 function publish() {
+    let nowTime = Date.now();
+    if (!isPost) {
+        // 一度も投稿していない場合
+        last_send = nowTime;
+        isPost = true;
+    } else {
+        console.log(nowTime - last_send)
+        if (nowTime - last_send < interval_time) {
+            alert('投稿間隔が短すぎます');
+            return false;
+        } else {
+            last_send = nowTime;
+        }
+    }
+
     // ユーザ名を取得
     const userName = $('#userName').val();
     // 入力されたメッセージを取得
