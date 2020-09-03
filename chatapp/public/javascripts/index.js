e9b9b6bb7442655e2bdadcc808c06a67946d9036
takumidev86 <takumidevelopment86@gmail.com>
2020-09-03 11:23:37 +0900

    [基本機能] ログイン機能

--- chatapp/public/javascripts/index.js
+++ chatapp/public/javascripts/index.js
@@ -3,9 +3,13 @@
 // チャットルームに入室する
 function enter() {
     // 入力されたユーザ名を取得する
-    const userName = '';
+    const userName = $('#userName').val();
     // ユーザ名が未入力でないかチェックする
+    if (!userName.trim()) {
+        alert("ユーザ名が未入力です。");
+    } else {
+        $('form').submit();
+    }
 
 
-    $('form').submit();
 }
