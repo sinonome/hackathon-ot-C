9792c94fc3b57667879a1badcad2740229de9d3a
takumidev86 <takumidevelopment86@gmail.com>
2020-09-03 20:16:34 +0900

    [追加機能1 3]

--- chatapp/public/javascripts/publish.js
+++ chatapp/public/javascripts/publish.js
@@ -8,17 +8,22 @@ function publish() {
     const message = $('#message').val();
     const sendMessage = {
         'userName' : userName,
-        'message' : message
+        'message' : message,
+        'time': new Date().toLocaleString(),
     };
-    // 投稿内容を送信
-    // alert('OK' + userName + ":" + message);
-    socket.emit("sendMessageEvent", sendMessage);
-    return false;
+    if (!sendMessage['message'].trim()) {
+        alert('投稿文が未入力です');
+    }
+    else {
+        socket.emit("sendMessageEvent", sendMessage);
+        $('#message').val("");
+        return false;
+    }
 }
 
 // サーバから受信した投稿メッセージを画面上に表示する
 socket.on('getMessageEvent', function (data) {
     console.log(data);
     console.log(typeof data);
-    $('#thread').prepend('<p>' + data['userName'] + ' : ' + data['message'] + '</p>');
+    $('#thread').prepend('<p>' + data['time'] + ' : '+ data['userName'] + ' : ' + data['message'] + '</p>');
 });
