d12df4472c6257ea89bbb08e1cc45c81a08eba2a
takumidev86 <takumidevelopment86@gmail.com>
2020-09-04 02:11:06 +0900

    [追加機能1&3] メモ機能に未入力判定、時間表示を追加

--- chatapp/public/javascripts/memo.js
+++ chatapp/public/javascripts/memo.js
@@ -3,11 +3,25 @@
 // メモを画面上に表示する
 function memo() {
     // ユーザ名を取得
-    const userName = $('#userName').val();
+    // const userName = $('#userName').val();
     // 入力されたメッセージを取得
-    const message = $('#message').val();
+    // const message = $('#message').val();
+
+    const memoData = {
+        'userName': $('#userName').val(),
+        'memo': $('#message').val(),
+        'time': new Date().toLocaleString(),
+    }
+
     // メモの内容を表示
-    $('#thread').prepend('<p>' +userName+'さんのメモ：'+message+ '</p>');
+    // $('#thread').prepend('<p>' +userName+'さんのメモ：'+message+ '</p>');
+    if (!memoData['memo'].trim()) {
+        alert('メモが未入力です。');
+    }
+    else {
+        $('#thread').prepend('<p>' + memoData['time'] + ' : ' + memoData['userName'] + "さんのメモ : " + memoData['memo'] + '</p>');
+        $('#message').val("");
+    }
 
     return false;
 }
