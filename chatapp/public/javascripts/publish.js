08a5c3e39a6b6492d13697b019b2c9536dcef078
pis <pis.ceyatta@gmail.com>
2020-09-03 19:55:22 +0900

    チャット送信機能実装

--- chatapp/public/javascripts/publish.js
+++ chatapp/public/javascripts/publish.js
@@ -3,15 +3,22 @@
 // 投稿メッセージをサーバに送信する
 function publish() {
     // ユーザ名を取得
-    const userName = '';
+    const userName = $('#userName').val();
     // 入力されたメッセージを取得
-    const message = '';
+    const message = $('#message').val();
+    const sendMessage = {
+        'userName' : userName,
+        'message' : message
+    };
     // 投稿内容を送信
-
+    // alert('OK' + userName + ":" + message);
+    socket.emit("sendMessageEvent", sendMessage);
     return false;
 }
 
 // サーバから受信した投稿メッセージを画面上に表示する
-socket.on('', function (data) {
-    $('#thread').prepend('<p>' + '</p>');
+socket.on('getMessageEvent', function (data) {
+    console.log(data);
+    console.log(typeof data);
+    $('#thread').prepend('<p>' + data['userName'] + ' : ' + data['message'] + '</p>');
 });
