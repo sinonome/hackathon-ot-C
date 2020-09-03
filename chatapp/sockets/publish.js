c42de68fe4e3687323b89b49795cbb30acb7dcd7
pis <pis.ceyatta@gmail.com>
2020-09-03 20:26:25 +0900

    delete log

--- chatapp/sockets/publish.js
+++ chatapp/sockets/publish.js
@@ -3,7 +3,6 @@
 module.exports = function (socket, io) {
     // 投稿メッセージを送信する
     socket.on('sendMessageEvent', function (data) {
-        console.log(data);
         io.sockets.emit('getMessageEvent', data);
     });
 };
