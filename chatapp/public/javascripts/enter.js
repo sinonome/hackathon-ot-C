0f1b6457b16503531f982c87c280e4f574dfaf07
KokoNishiura <kokomeron1028@gmail.com>
2020-09-04 14:02:24 +0900

    enterキーの処理の修正

--- chatapp/public/javascripts/enter.js
+++ chatapp/public/javascripts/enter.js
@@ -3,11 +3,9 @@
 // 入室メッセージをサーバに送信する
 // 入力されたユーザ名を取得する
 const userName = $("#userName").val();
-// const userName = '';
 // 入室メッセージイベントを送信する
 
 socket.emit("sendUserNameEvent", userName);
-
 
 // サーバから受信した入室メッセージを画面上に表示する
 socket.on('receiveEnterEvent', function (data) {
