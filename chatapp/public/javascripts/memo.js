a45aece37092719ddcb5fa9c4add1fb19075a29f
KokoNishiura <kokomeron1028@gmail.com>
2020-09-05 10:25:35 +0900

    [追加機能8]表示の切り替え処理の追加

--- chatapp/public/javascripts/memo.js
+++ chatapp/public/javascripts/memo.js
@@ -2,10 +2,6 @@
 
 // メモを画面上に表示する
 function memo() {
-    // ユーザ名を取得
-    // const userName = $('#userName').val();
-    // 入力されたメッセージを取得
-    // const message = $('#message').val();
 
     const memoData = {
         'userName': $('#userName').val(),
@@ -14,7 +10,6 @@ function memo() {
     }
 
     // メモの内容を表示
-    // $('#thread').prepend('<p>' +userName+'さんのメモ：'+message+ '</p>');
     if (!memoData['memo'].trim()) {
         alert('メモが未入力です。');
     }
