1759dc4bb10bbadfdeb05d72fef79763665da837
pis <pis.ceyatta@gmail.com>
2020-09-17 15:34:12 +0900

    右クリック実装

--- chatapp/public/javascripts/click.js
+++ chatapp/public/javascripts/click.js
@@ -1,18 +1,5 @@
 window.onload = function() {
-    document.getElementById("thread").addEventListener('contextmenu', function(e){
-        // マウスの位置を style へ設定
-        document.getElementById('contextmenu').style.left = e.clientX+"px";
-        document.getElementById('contextmenu').style.top = (
-            e.clientY
-            - parseInt(($("div").css("margin-top")))
-            + "px"
-        );
-        document.getElementById('contextmenu').style.display = "block";
-    });
-    document.body.addEventListener("click", function(e) {
-        // メニューを非表示
-        document.getElementById('contextmenu').style.display = "none";
-    })
+    document.body.addEventListener("click", Click.close);
 };
 
 // 対象の投稿をデリート
@@ -24,6 +11,6 @@ function deleteMessage() {
             3. 削除リクエストが通ったらその投稿を削除
             4. その際何かアクションを起こしていいかもしれない？
     */
-    console.log("click: delete");
+    eventDOM.remove();
     return false;
 };
